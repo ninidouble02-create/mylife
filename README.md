8aed42226c34b93f5d79af7fef7c0e6fd71ed6ec
ninidouble02-create <ninidouble02@gmail.com>
2026-08-19 13:22:03 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# My Life OS PWA v0.7
+# My Life OS PWA v0.8
 
 稳定修复版：修复 iPhone Safari/PWA 下数据保存与 DOM 元素绑定导致的空白卡片和按钮无响应问题；保留 Plan 月历、Archive、Health 趋势、Money 总结、Habit 热力图、Knowledge 图片等功能。
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-life-os-v0.7-20260819';
+const CACHE='my-life-os-v0.8-20260819';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
